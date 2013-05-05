ddcedea102eb2dfa85c51c551e066b8a6c4c45d3
Mike Chau <mchau@iwu.edu>
2013-05-05 03:54:03 -0500

    Remove translation from helper

--- app/helpers/spree/inquiries_helper.rb
+++ app/helpers/spree/inquiries_helper.rb
@@ -2,12 +2,12 @@ module Spree::InquiriesHelper
   def inquiry_types_translated
     types = Spree::ContactUsConfiguration[:inquiry_types].dup
 
-    unless types.respond_to? :collect
-      types = YAML.load Spree::ContactUsConfiguration[:inquiry_types]
-    end
+    # unless types.respond_to? :collect
+    #   types = YAML.load Spree::ContactUsConfiguration[:inquiry_types]
+    # end
 
     # Translate types, use string for missing translations
-    types.collect { |i| t(i, :default => i.to_s) }
+    types.collect { |i| i.to_s }
 
   end
 end
